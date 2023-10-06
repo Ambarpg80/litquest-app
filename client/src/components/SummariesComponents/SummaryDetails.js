411c317868ca393b058ae843909fbd59fa07fd81
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-10-05 22:38:46 -0400

    add Summaries Components folder for summary lists, details and form components

---
+++ client/src/components/SummariesComponents/SummaryDetails.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SummaryDetails(){
+    return(
+       <div >
+        {/* 
+          <p>{summary}</p> 
+          <button>Edit Summary</button>
+          <button>Delete Summary</button>
+        */}
+       </div>
+    )
+}
+export default SummaryDetails;
---
+++ client/src/components/SummariesComponents/SummaryList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SummaryList(){
+    return(
+       <div >
+        {/* summaries.map(summary => <div key={summary.id}><SummaryDetails summary={summary}/></div>) */}
+       </div>
+    )
+}
+
+export default SummaryList;
